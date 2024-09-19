42429385e34990387ab52b44b9574199d372670e
Mr-Trissel <Trissel.management@gmail.com>
2024-09-18 17:09:26 -0700

    Add robot.py and robotContainer with basic structure

---
+++ robot.py
@@ -0,0 +1,40 @@
+import wpilib
+import commands2
+
+# This Imports Files. Required for referencing. 
+
+
+class MyRobot(wpilib.TimedRobot):
+    def robotInit(self):
+        """
+        This function is called upon program startup and
+        should be used for any initialization code.
+        """
+        pass
+
+    def autonomousInit(self):
+        """This function is run once each time the robot enters autonomous mode."""
+        pass
+
+    def autonomousPeriodic(self):
+        """This function is called periodically during autonomous."""
+        pass
+
+    def teleopInit(self):
+        """This function is called once each time the robot enters teleoperated mode."""
+        pass
+    def teleopPeriodic(self):
+        """This function is called periodically during teleoperated mode."""
+        pass
+
+    def testInit(self):
+        """This function is called once each time the robot enters test mode."""
+        pass
+
+    def testPeriodic(self):
+        """This function is called periodically during test mode."""
+        pass
+
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
